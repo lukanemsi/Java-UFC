d921be90e5b3ef2a8f78db6c273d48e205618dda
lukanemsi <94932118+lukanemsi@users.noreply.github.com>
2022-06-02 17:29:23 +0400

    commit

---
+++ HW5/company/src/com/company/src/com/company/ufc/SquareChild.java
@@ -0,0 +1,7 @@
+package com.company.ufc;
+
+public class SquareChild //extends Square
+{
+            /* -> compile error  რადგან Figure კლასი გამოცხადებულია როგორც Final
+                             შესაბამისად მემკვიდრე ვერ ეყოლება */
+}
--- HW5/company/src/com/company/ufc/FigureChild.java
+++
@@ -1,9 +0,0 @@
-package com.company.ufc;
-
-import com.company.Figure;
-
-public class FigureChild // extends Figure
-{
-                        /* -> compile error  რადგან Figure კლასი გამოცხადებულია როგორც Final
-                         შესაბამისად მემკვიდრე ვერ ეყოლება */
-}
