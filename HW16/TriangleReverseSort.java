e9d733050de124103511db506ac66c25fd5b7b7e
lukanemsi <94932118+lukanemsi@users.noreply.github.com>
2022-07-07 17:40:00 +0400

    commit

--- HW16/TriangleReverseSort.java
+++ HW16/TriangleReverseSort.java
@@ -4,7 +4,10 @@ import java.util.Comparator;
 
 public class TriangleReverseSort implements Comparator<Triangle> {
     @Override
-    public int compare(Triangle o1, Triangle o2) {
+    public int compare(Triangle o1, Triangle o2) 
+	{
+		if(o1 == null || o2 == null)
+			return 0;
         return -Double.compare(o1.perimeter(),o2.perimeter());
     }
 }
